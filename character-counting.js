966a674144ce6cb7642ebb9d81c462d03b3a9d3f
Nina Yujiri <ninayujiri@gmail.com>
2018-04-11 17:44:35 +0000

    cleaned up code

--- character-counting.js
+++ character-counting.js
@@ -1,20 +1,14 @@
 function countLetters(str) {
-
   let noSpaces = str.split(" ").join("").split("");
   let result = {};
-
   for (var i = 0; i < noSpaces.length; i++) {
-
     if (!result[noSpaces[i]]) {
       result[noSpaces[i]] = 1;
     } else {
       result[noSpaces[i]]++;
     }
-
   }
-
   return result;
-
 }
 
 console.log(countLetters("lighthouse in the house"));
